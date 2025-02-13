ad08c51309fb17819454c09b7d114ab6e986338a
happy-spark <107300540+happy-spark@users.noreply.github.com>
2025-02-13 23:58:24 +0900

    Update calculate-PI-by-Monte-Carlo-method.jl

--- calculate-PI-by-Monte-Carlo-method.jl
+++ calculate-PI-by-Monte-Carlo-method.jl
@@ -3,7 +3,6 @@ number_of_point = parse(Int, ARGS[1])
 not_inside_circle = 0
 
 for i in 1:number_of_point
-  global not_inside_circle
   if ( (2*rand() - 1)^2 ) + ( (2*rand() - 1)^2 ) > 1
     not_inside_circle = not_inside_circle + 1
   end
