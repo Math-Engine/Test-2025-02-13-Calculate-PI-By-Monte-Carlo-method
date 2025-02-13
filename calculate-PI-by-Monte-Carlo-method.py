bc01a9ef2d7bd34eedde712362c0303f92e1aa47
happy-spark <107300540+happy-spark@users.noreply.github.com>
2025-02-13 13:47:23 +0900

    Create calculate-PI-by-Monte-Carlo-method.py

---
+++ calculate-PI-by-Monte-Carlo-method.py
@@ -0,0 +1,22 @@
+import sys
+sys.set_int_max_str_digits(2147483647)
+
+number_of_point = int(sys.argv[1])
+
+from decimal import Decimal, getcontext
+from fractions import Fraction
+
+import random
+
+
+inside_circle = 0
+for i in range(0, number_of_point):
+  coordinates = [random_number = random.uniform(0, 2), random_number = random.uniform(0, 2)]
+  print(coordinates)
+  if ( (((coordinates[0] - 1)**2) + ((coordinates[1] - 1)**2)) <= 1 ):
+    inside_circle = inside_circle + 1
+
+print(f"number_of_point : {number_of_point}")
+print(f"inside_circle : {inside_circle}")
+
+fraction = Fraction(inside_circle * 4, number_of_point)
